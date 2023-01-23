84c14ecce4c251f1f50815680f7ae8b72021fd37
SwethaKoppuravuri <119507872+SwethaKoppuravuri@users.noreply.github.com>
2023-01-23 07:59:03 +0530

    check switch with default

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,21 @@
 class Main {
     public static void main(String[] args) {
 
-        int expression = 2;
+        int expression = 9;
 
-        switch (expression) {
-            case 1:
-                System.out.println("Case 1");
+        switch(expression) {
 
             case 2:
-                System.out.println("Case 2");
+                System.out.println("Small Size");
+                break;
 
             case 3:
-                System.out.println("Case 3");
+                System.out.println("Large Size");
+                break;
 
+            // default case
             default:
-                System.out.println("Default case");
+                System.out.println("Unknown Size");
         }
     }
 }
